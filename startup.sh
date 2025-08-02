1b05c08490bfb11d1e0cc7826cdf1b877aa86027
Jesse Ellis - Cloud Potions <34029756+cloudpotions@users.noreply.github.com>
2025-08-02 19:18:04 -0400

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start X virtual framebuffer
+Xvfb :1 -screen 0 1920x1080x24 &
+
+# Start desktop environment
+su - zorin -c 'startxfce4 &'  # Zorin uses GNOME-based but XFCE works reliably in container
+
+# Start VNC
+x11vnc -display :1 -usepw -forever -shared -rfbport 5900 &
+
+# Start noVNC
+/opt/novnc/utils/launch.sh --vnc localhost:5900 --listen 8080
